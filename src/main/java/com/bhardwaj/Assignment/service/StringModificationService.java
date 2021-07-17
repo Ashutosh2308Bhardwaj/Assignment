dcdfdf15146ecc7465d2de132a53aac46e4d458c
ashutoshbhardwaj <ashutosh.bhardwaj@oyorooms.com>
2021-07-17 13:38:49 +0530

    Added comments for string modification logic

--- src/main/java/com/bhardwaj/Assignment/service/StringModificationService.java
+++ src/main/java/com/bhardwaj/Assignment/service/StringModificationService.java
@@ -22,28 +22,33 @@ public class StringModificationService {
 
     public void removeSimilarAdjacentCharacters(){
 
-        List<InputDetail> inputDetails = inputDetailsRepository.findAll();
+        List<InputDetail> inputDetails = inputDetailsRepository.findAll();   // Get all data in input_details table
 
         List<OutputDetail> outputDetails = new ArrayList<>();
 
         for(InputDetail inputDetail : inputDetails){
-            String input = inputDetail.getInputString();
-            StringBuilder output = new StringBuilder();
+
+            String input = inputDetail.getInputString();    // Original String
+
+            StringBuilder output = new StringBuilder();     // modifiedString stringBuilder
+
             Integer count=0;
+
             for (char c : input.toCharArray()) {
                 int size = output.length();
-                if (size > 0 && output.charAt(size - 1) == c) {
-                    count++;
-                    output.deleteCharAt(size - 1);
+                if (size > 0 && output.charAt(size - 1) == c) {    // Check if current character is same as last character of output string formed till now
+                    count++;                                       // increment count
+                    output.deleteCharAt(size - 1);                 // remove same adjacent characters
                 } else {
                     output.append(c);
                 }
             }
+
             OutputDetail outputDetail = new OutputDetail(input,output.toString(),count,new Timestamp(System.currentTimeMillis()));
             outputDetails.add(outputDetail);
         }
 
-        outputDetailsRepository.saveAll(outputDetails);
+        outputDetailsRepository.saveAll(outputDetails);         // insert data in ashutosh_bhardwaj_java_output table
 
     }
 
